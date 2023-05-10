7d6a4c4467fc7a664e19bb3121632d8fad70fcd8
Sanflofer <>
2023-05-10 18:27:27 +0000

    description of cities and map

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+//maps//
+function initMap() {
+    var leonMap = new google.maps.map(document.GetElementByID("leonMap"), {
+        zoom: 3;
+        center: {
+            lat: 42.;
+            lng: -5.57;
+        }
+    });
+}
+
+
+
+AIzaSyBtvBA__0wOwONjiyUl7frDIyGL7dlQZ60;
